96d69570a8e6f9f91e81ee99cf3ffce3cec64c1b
David <goyaldavid55@gmail.com>
2024-09-29 11:12:57 +0530

    Size decreased

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Loading = () => {
 	return (
 		<div className="w-screen h-screen flex justify-center items-center bg-black">
-			<div className="loader border-t-transparent border-white border-4 rounded-full h-[12vw] w-[12vw] animate-spin" />
+			<div className="loader border-t-transparent border-white border-4 rounded-full h-[10vw] w-[10vw] animate-spin" />
 		</div>
 	);
 };
